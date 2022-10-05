a2029e7ccaba910ecfa7f4f4a9d9f120909731e1
unknown <ninja333@list.ru>
2022-10-05 11:19:00 +0900

    Задача 2.3.1

--- src/main/java/web/Service/UserServiceImp.java
+++ src/main/java/web/Service/UserServiceImp.java
@@ -3,7 +3,6 @@ package web.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
-import web.dao.UserDaoImp;
 import web.model.User;
 import java.util.List;
 
